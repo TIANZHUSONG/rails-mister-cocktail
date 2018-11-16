897b374ac19adaecbe5029cb6afcf1bfff468de4
tianzhu song <songtianzhu@gmail.com>
2018-11-16 21:55:14 +0800

    finshed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only:[:new, :create, :destroy]
   end
 
-  root to: 'cocktails#index'
 end
+
